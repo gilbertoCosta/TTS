2a098525f854c588af6f024b447a3d1279c0749a
GILBERTO COSTA JUNIOR <gilberto.costa@fatec.sp.gov.br>
2020-03-25 18:53:51 -0300

    initial commit

---
+++ app/src/main/java/app/f/d/voz/MainActivity.java
@@ -0,0 +1,169 @@
+package app.f.d.voz;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG_LOG = "LOG_TTS";
+
+    String phoneNumber = "981981810";
+    String message = "Texto padrão para envio do SMS.";
+
+
+    private TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView (R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.microfone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                catchSpeech();
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+    }
+
+   @Override
+   public void onDestroy(){
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+   }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_sair) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void pause(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        }catch (Exception e){
+            Log.e(TAG_LOG,"Erro gerarPausa(): "+e.getMessage());
+        }
+    }
+
+    private void catchSpeech() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Fale naturalmente...");
+
+        try{
+            startActivityForResult(intent, 10);
+        }catch (ActivityNotFoundException a){
+            Toast.makeText(this,"Reconhecimento de voz não suportado",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 10){
+
+            if (resultCode == RESULT_OK && null != data) {
+
+                ArrayList<String> result =
+                        data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                String speech = result.get(0);
+
+                processMachineLearning(speech);
+            }
+        }
+    }
+
+    private void processMachineLearning(String speech) {
+
+        if(speech.toUpperCase().contains("DESLIGAR")){
+            pause(2);
+            finish();
+        }
+
+        if(speech.toUpperCase().contains("SMS")){
+            sendSMS(phoneNumber, message);
+        }
+
+        if(speech.toUpperCase().contains("GOVERNO BRASIL")){
+            openURL();
+        }
+
+        if(speech.toUpperCase().contains("ABRIR CÂMERA")){
+            openCamera();
+        }
+    }
+
+    private void openURL() {
+        String URL = "http://www.brasil.gov.br";
+
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(URL));
+        startActivity(intent);
+    }
+
+    private void sendSMS(String telephone, String message) {
+        Uri uri = Uri.parse("tel:" + telephone);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.putExtra("address", telephone);
+        intent.putExtra("sms_body", message);
+        intent.setType("vnd.android-dir/mms-sms");
+        startActivity(intent);
+    }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 1);
+        }
+    }
+
+
+}
